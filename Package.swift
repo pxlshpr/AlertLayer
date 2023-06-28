2a6c6549d9b050075c2f44ead4ec65a222c36f08
Ahmed Khalaf <pxlshpr@gmail.com>
2023-06-28 13:33:17 +0500

    first actual commit

--- Package.swift
+++ Package.swift
@@ -5,17 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "AlertLayer",
+    platforms: [
+        .iOS(.v17)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AlertLayer",
             targets: ["AlertLayer"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/pxlshpr/SwiftSugar", from: "0.0.97")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AlertLayer"),
+            name: "AlertLayer",
+            dependencies: [
+                .product(name: "SwiftSugar", package: "SwiftSugar"),
+            ]
+        ),
         .testTarget(
             name: "AlertLayerTests",
             dependencies: ["AlertLayer"]),
--- Sources/AlertLayer/AlertLayer.swift
+++ Sources/AlertLayer/AlertLayer.swift
@@ -1,2 +1,148 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+import SwiftSugar
+
+//TODO: Revisit animations
+/// [ ] Put in `phaseAnimator` and still not sure what's going on. Revisit it after watching WWDC videos. It works for now though.
+public struct AlertLayer: View {
+
+    @Binding var message: String
+    @Binding var showingAlert: Bool
+    
+    @State private var internalShowingAlert: Bool = false
+    @State private var presentTask: Task<Void, Error>? = nil
+    @State private var presentAnimationStep = 0
+    
+    private let BottomRadius: CGFloat = 10
+    private let MaxWidth: CGFloat = 500
+    private let ContentHeight: CGFloat = 50
+    private let HorizontalPadding: CGFloat = 10
+    private let OffscreenYOffset: CGFloat = 120
+
+    public var body: some View {
+        GeometryReader { proxy in
+            VStack {
+                alert(proxy)
+                Spacer()
+            }
+        }
+        .onChange(of: showingAlert, showingAlertChanged)
+    }
+    
+    enum PresentAnimationPhase: CaseIterable {
+        case start, middle, end
+    }
+    
+    private func showingAlertChanged(oldValue: Bool, newValue: Bool) {
+        guard newValue == true else { return }
+        showingAlert = false
+        presentTask?.cancel()
+        presentTask = Task {
+            await MainActor.run {
+//                withAnimation(.bouncy) {
+                withAnimation {
+                    internalShowingAlert = true
+                }
+            }
+            
+            try await sleepTask(3, tolerance: 0.1)
+            
+            await MainActor.run {
+//                withAnimation(.snappy) {
+                withAnimation(.snappy) {
+                    internalShowingAlert = false
+                }
+            }
+        }
+        
+    }
+
+    private var background: some View {
+        var radii: RectangleCornerRadii {
+            RectangleCornerRadii(
+                topLeading: 0,
+                bottomLeading: BottomRadius,
+                bottomTrailing: BottomRadius,
+                topTrailing: 0
+            )
+        }
+        
+//        return UnevenRoundedRectangle(cornerRadii: radii, style: .continuous)
+//            .fill(Color.accentColor)
+        
+        return VStack(spacing: 0) {
+            Spacer(minLength: 0)
+//            Rectangle()
+//                .fill(Color.accentColor.opacity(0.2))
+//            UnevenRoundedRectangle(cornerRadii: radii, style: .continuous)
+            RoundedRectangle(cornerRadius: BottomRadius, style: .continuous)
+                .fill(Color.accentColor)
+                .frame(height: ContentHeight)
+        }
+    }
+    
+    private var content: some View {
+        VStack(spacing: 0) {
+            Spacer(minLength: 0)
+            HStack {
+                Image(systemName: "checkmark.circle.fill")
+                Text(message)
+//                    .font(.subheadline)
+                    .fontWeight(.semibold)
+            }
+            .foregroundStyle(Color(.systemBackground))
+            .frame(height: ContentHeight)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading, 10)
+        }
+    }
+    
+    private func alert(_ proxy: GeometryProxy) -> some View {
+        HStack {
+            Spacer(minLength: HorizontalPadding)
+            ZStack {
+                background
+                content
+            }
+            .frame(maxWidth: MaxWidth, alignment: .center)
+            Spacer(minLength: HorizontalPadding)
+        }
+        .frame(height: ContentHeight + proxy.safeAreaInsets.top)
+        .padding(.horizontal, HorizontalPadding)
+        
+//        .offset(y: offsetY(proxy))
+        
+        .phaseAnimator(PresentAnimationPhase.allCases, trigger: presentAnimationStep) { content, phase in
+            content
+                .offset(y: offsetY(proxy, phase: phase))
+        } animation: { phase in
+            switch phase {
+            case .start, .middle: .easeInOut
+            case .end: .bouncy
+            }
+        }
+    }
+    
+    private func offsetY(_ proxy: GeometryProxy) -> CGFloat {
+        var offset = -proxy.safeAreaInsets.top
+        if !internalShowingAlert {
+            offset -= OffscreenYOffset
+        }
+        return offset
+    }
+    
+    private func offsetY(_ proxy: GeometryProxy, phase: PresentAnimationPhase) -> CGFloat {
+        var offset = -proxy.safeAreaInsets.top
+        if !internalShowingAlert {
+            switch phase {
+            case .start:
+                offset -= OffscreenYOffset
+            case .middle:
+                offset -= (OffscreenYOffset / 2.0)
+            case .end:
+                offset -= OffscreenYOffset
+            }
+        }
+        return offset
+    }
+
+}
